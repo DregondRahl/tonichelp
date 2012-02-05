d6e3c7f00aa5a9daa5374821f8528c787ac01cbc
Isern Palaus <ipalaus@ipalaus.es>
2012-02-05 21:28:41 +0100

    Moved the app folder outside of the fuel dir, added the installer dir and the index.php will check if it exists to throw an error if it's configured and not deleted.

--- src/tonichelp/config/development/db.php
+++ src/tonichelp/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=tonichelp_dev',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => 'mysql',
 		),
 	),
 );
---
+++ src/tonichelp/config/tonichelp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * TonicHelp is a opensource support ticket system/helpdesk.
+ *
+ * @package    TonicHelp
+ * @version    1.0
+ * @author     TonicHelp Development Team
+ * @license    -- to be decided --
+ * @copyright  2012 TonicHelp Development Team
+ * @link       http://tonichelp.com
+ */
+
+return array(
+
+	
+
+);

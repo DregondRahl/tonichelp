5a5aa0f4da51f15380a8a9881605c7ee8e98f673
Isern Palaus <ipalaus@ipalaus.es>
2012-02-23 01:41:29 +0100

    Removed not needed files.

--- src/tonichelp/classes/controller/welcome.php
+++ src/tonichelp/classes/controller/welcome.php
@@ -11,30 +11,6 @@
  */
 class Controller_Welcome extends BaseController
 {
-
-	/**
-	 * The basic welcome message
-	 * 
-	 * @access  public
-	 * @return  Response
-	 */
-	public function action_index()
-	{
-		return Response::forge(View::forge('welcome/index'));
-	}
-
-	/**
-	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
-	 * show how to use them.
-	 * 
-	 * @access  public
-	 * @return  Response
-	 */
-	public function action_hello()
-	{
-		return Response::forge(ViewModel::forge('welcome/hello'));
-	}
-
 	/**
 	 * The 404 action for the application.
 	 * 
@@ -43,6 +19,6 @@ class Controller_Welcome extends BaseController
 	 */
 	public function action_404()
 	{
-		return Response::forge(ViewModel::forge('welcome/404'), 404);
+		return Response::forge(ViewModel::forge('404'), 404);
 	}
 }
